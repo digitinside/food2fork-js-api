1b059042002303a7feaf90620de5b6bb477a880f
digitinside <temdemcom@gmail.com>
2019-11-11 13:39:09 +0100

    Added recipe search function

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
-import str from './models/Search';
-// import { add as a, multiply as m, ID } from './views/searcheView';
-import * as searcheView from './views/searcheView';
+import Search from './models/Search';
 
-// console.log(`Using imported functions! ${a(ID, 2)} and ${m(3, 5)}. ${str}`);
-console.log(`Using imported functions! ${searcheView.add(searcheView.ID, 2)} and ${searcheView.multiply(3, 5)}. ${str}`);
+const search = new Search('pizza'); 
+
+console.log(search);
+search.getResults();

904b400de043ec22c3ab69dceb1bc923cc0c5d07
digitinside <demirelt@yahoo.fr>
2019-11-11 13:39:09 +0100

    Added recipe search function

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,18 @@
-export default 'Exported string';
+import axios from 'axios';
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        try {
+            const key = 'd66111f071be4f70e5b7fd4118752c74';
+            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            //console.log(this.result);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+}

f97d8f5bae8485525874a6c11ffc62822bfe986b
digitinside <temdemcom@gmail.com>
2019-11-11 13:02:15 +0100

    Using ES6 module import

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
-// Global app controller
-import x from './test';
+import str from './models/Search';
+// import { add as a, multiply as m, ID } from './views/searcheView';
+import * as searcheView from './views/searcheView';
 
-const y = 222;
-console.log(`Imported variable ${x} from another module! Variable y = ${y} `);
+// console.log(`Using imported functions! ${a(ID, 2)} and ${m(3, 5)}. ${str}`);
+console.log(`Using imported functions! ${searcheView.add(searcheView.ID, 2)} and ${searcheView.multiply(3, 5)}. ${str}`);
